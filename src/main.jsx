1add4130aa26ee35dc31a2c17222f303371bd8a2
UditMathurWEBD <66465599+UditMathurWEBD@users.noreply.github.com>
2025-05-30 11:48:23 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { RouterProvider , createBrowserRouter } from 'react-router-dom'
 import ErrorPage from './components/404page.jsx'
-import 'app.css'
 import { lazy, Suspense } from 'react'
 
 
