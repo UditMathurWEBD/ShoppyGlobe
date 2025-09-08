745e4fe4acda6dd190500ec5cbe03bce09aef4b9
UditMathurWEBD <mathurudit90@gmail.com>
2025-09-08 12:33:57 +0530

    Filters added

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -8,13 +8,48 @@ export default function ProductList() {
 
 const { data, loading, error } = useFetchProducts('https://dummyjson.com/products?limit=100');
 const [filteredData,setFilteredData] = useState([]);
+const [categories,setCategories] = useState([]);
+const [minPrice, setMinPrice] = useState("");
+const [maxPrice, setMaxPrice] = useState("");
+
+
 
 useEffect(()=>{
   if(data){
   setFilteredData(data)
+ const uniqueCategories = [...new Set(data.map(item => item.category))];
+  setCategories(uniqueCategories);
+  console.log(categories)
+
   }
 
+
 },[data])
+
+
+function handlePriceFilter() {
+  const min = parseFloat(minPrice) || 0;
+  const max = parseFloat(maxPrice) || Number.MAX_VALUE;
+
+  const newData = data.filter(
+    (item) => item.price >= min && item.price <= max
+  );
+
+  setFilteredData(newData);
+}
+
+
+function handleFilter(e){
+  const selected = e.target.value;
+  if(selected == "all"){
+    setFilteredData(data);
+  }else{
+  const newData = data.filter((item)=> item.category == selected)
+  setFilteredData(newData)
+  }
+
+
+}
 
 function handleSearch(event){
   // console.log(event.target.value)
@@ -43,8 +78,35 @@ function handleSearch(event){
 <p className="mt-2 text-md text-gray-600  sm:max-w-[60%] max-w-[100%]">
   Discover and shop from a wide range of products—all in one place. Add items to your cart, explore detailed product info, and enjoy a seamless checkout experience with <strong>ShoppyGlobe</strong>.
 </p>
-<div className="mb-20 mt-6">
+<div className="mb-20 mt-6 flex justify-center items-center gap-4">
   <input onChange={handleSearch} className="p-4 border rounded-md text-gray-600 w-[-webkit-fill-available]" type="text" placeholder="Search for products"></input>
+<div className="h-[-webkit-fill-available] place-content-center bg-gray-100 px-5 rounded-md">
+<select onChange={handleFilter} name="Category" id="category">
+  <option value="all">All</option>
+  {categories.map((c)=>{
+    return  <option value={c}>{c}</option>
+  })}
+</select>
+</div>
+<div className="flex gap-4 items-center  bg-gray-100 h-[-webkit-fill-available] px-2 rounded-md">
+  <input
+    type="number" 
+    placeholder="Min Price"
+    onChange={(e) => setMinPrice(e.target.value)}
+    value={minPrice}
+    className="p-2 border rounded bg-white"
+  />
+  <input
+    type="number"
+    placeholder="Max Price"
+    onChange={(e) => setMaxPrice(e.target.value)}
+    value={maxPrice}
+    className="p-2 border rounded  bg-white"
+  />
+  <button onClick={handlePriceFilter} className="p-2 bg-gray-800 text-white rounded">
+    Apply
+  </button>
+</div>
 </div>
 
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
